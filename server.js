1eedb29aa339fa3515c1b9006fa0b895420353b6
Andrés Toledano <andres13072001@gmail.com>
2023-01-31 09:42:41 +0100

    Ahora se pueden leer las cookies y eliminarlas

--- server.js
+++ server.js
@@ -10,8 +10,25 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/setcookie', (req,res)=>{
-    res.cookie('my cookie name', 'my cookie')
+    res.cookie('my cookie name', 'my cookie',{
+        expires: new Date('2023-12-31'),
+        //maxAge:7000,
+        httpOnly:true,
+        secure: true,
+        sameSite:'lax'
+    })
     res.send('Página principal del manejo de cookies')
+})
+
+app.get('/getcookies',(req,res)=>{
+    console.log(req.cookies)
+    res.send('Reading cookies')
+})
+
+app.get('/deletecookies',(req,res)=>{
+    console.log(req.cookies)
+    res.clearCookie('my cookie name')
+    res.send('Reading cookies')
 })
 
 app.listen(3000)
